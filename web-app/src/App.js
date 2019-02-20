101f5cead86c809001cbd44c73aacd2f5b1a21fa
Daniel Gomez <dggomez21@gmail.com>
2019-02-20 10:17:24 -0500

    Update App.js

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import axios from 'axios';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
+import {connect} from "react-redux";
+
+import DashboardPage from "./pages/MainPage";
+import ReportPage from "./pages/ReportPage";
+
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div className="container">
+          <Router>
+            <div style={{ marginTop: '7rem' }}>
+                <NavBar />
+                <Route path="/" exact component={DashboardPage} />
+                <Route path="/dashboard" exact component={DashboardPage} />
+                <Route path="/report" exact component={ReportPage} />
+                <Footer/>
+            </div>
+          </Router> 
+        </div>
     );
   }
 }
