f7298952771e428c69c857393c9465338453f598
Daniel Gomez <dggomez21@gmail.com>
2019-02-22 09:04:40 -0500

    Update unitTest.test.js

--- web-app/src/test/unitTest.test.js
+++ web-app/src/test/unitTest.test.js
@@ -1,1 +1,38 @@
-hello
+// mainPage.test.js
+import React from 'react';
+import DashboardPage from '../pages/MainPage.js';
+import ReportPage from '../pages/ReportPage.js';
+
+import Card from '../components/Card.js';
+import Footer from '../components/Footer.js';
+import NavBar from '../components/NavBar.js';
+
+import {shallow} from 'enzyme';
+
+describe("Testing page rendering", function(){
+
+  it('renders DashboardPage without crashing', () => {
+    shallow(<DashboardPage />);
+  });
+
+  it('renders  without crashing', () => {
+    shallow(<ReportPage />);
+  });
+    
+}
+     
+describe("Testing components rendering", function(){
+
+  it('renders DashboardPage without crashing', () => {
+    shallow(<Card title="test" location="123 test" description="something happened" />);
+  });
+
+  it('renders Footer without crashing', () => {
+    shallow(<Footer />);
+  });
+    
+  it('renders NavBar without crashing', () => {
+    shallow(<NavBar />);
+  }); 
+    
+}
