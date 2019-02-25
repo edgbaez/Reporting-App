4469f99b5befd14ede5ea90b430479ef3495a1a4
Daniel Gomez <dggomez21@gmail.com>
2019-02-25 09:09:12 -0500

    Update dataController.js

--- backend/controllers/dataController.js
+++ backend/controllers/dataController.js
@@ -1,23 +1,43 @@
-
+var connectionInfo = require('../db/db.js');
 
 var connection = mysql.createConnection(connectionInfo);
 
 exports.list_all = function(req, res) {
-  
+  connection.query(req, function(err,result){
+    if(err) throw err;
+    
+    return result;
+  }
 };
 
 exports.create = function(req, res) {
-  
+   connection.query(req, function(err,result){
+    if(err) throw err;
+    
+    return "Success";
+  }
 };
 
 exports.read = function(req, res) {
-   
+   connection.query(req, function(err,result){
+    if(err) throw err;
+    
+    return result;
+  }
 };
 
 exports.update = function(req, res) {
+   connection.query(req, function(err,result){
+    if(err) throw err;
     
+    return "Success";
+  }
 };
 
 exports.delete = function(req, res) {
+    connection.query(req, function(err,result){
+    if(err) throw err;
     
+    return "Success";
+  }
 };
