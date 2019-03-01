9470abcc4c8b9ef721d56c40aead268fac7da766
Daniel Gomez <dggomez21@gmail.com>
2019-03-01 08:38:49 -0500

    Update main.js

--- code-check/main.js
+++ code-check/main.js
@@ -1,7 +1,26 @@
 var cron = require('node-cron');
  
+var connectionInfo = require('../db/db.js');
+var connection = mysql.createConnection(connectionInfo);
+
 //sunday
 cron.schedule('5 8 * * 0', () => {
-  console.log('running a task every minute');
-  
+  console.log('running a task every week');
+  connection.query(`Select * FROM events WHERE location=$req.location`, function(err,result){
+    if(err) throw err;
+    
+    // read through data and look for key words
+   
+   for(var i =0; i<result.length; i++){
+    
+   }
+  }
 });
+ 
+ function search(title,location,description){
+  // if certain words come up tht are similar to other cases group them and set priority
+  
+ }
+ 
+ function push(title,location,description,priority){
+ }
