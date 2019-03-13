2b90ca5ce32f8a7bdf46a42ac0923c583d7708ee
EC2 Default User <ec2-user@ip-10-160-226-233.ec2.internal>
2019-03-13 13:00:09 +0000

    added create report page

--- mobile-app/screens/LinksScreen.js
+++ mobile-app/screens/LinksScreen.js
@@ -1,27 +1,87 @@
-import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import React, {Component} from 'react';
+import { ScrollView, StyleSheet, View, Button } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
+import t from 'tcomb-form-native';
 
+const Form = t.form.Form;
+
+const Report = t.struct({
+  title: t.String,
+  location: t.String,
+  description: t.String
+  
+});
+
+const formStyles = {
+  ...Form.stylesheet,
+  formGroup: {
+    normal: {
+      marginBottom: 10
+    },
+  },
+  controlLabel: {
+    normal: {
+      color: 'blue',
+      fontSize: 18,
+      marginBottom: 7,
+      fontWeight: '600'
+    },
+    // the style applied when a validation error occours
+    error: {
+      color: 'red',
+      fontSize: 18,
+      marginBottom: 7,
+      fontWeight: '600'
+    }
+  }
+}
+
+const options = {
+  fields: {
+    title: {
+      error: "It's okay if the title isn't that good just get information out there. Put anything here. Please and Thank you"
+    },
+    location: {
+      error: 'Location of event so we can warn other people. Please and Thank you.'
+    },
+    description: {
+      error: "If it's hard write a bit of it and come back and finish it later. You got this. Please and Thank you",
+    },
+  },
+  stylesheet: formStyles,
+};
 export default class LinksScreen extends React.Component {
+    handleSubmit = () => {
+    const value = this._form.getValue();
+    console.log('value: ', value);
+  }
+  
   static navigationOptions = {
-    title: 'Links',
+    title: 'Create Report',
   };
 
-  render() {
+render() {
     return (
-      <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
-      </ScrollView>
+      <View style={styles.container}>
+        <Form 
+          ref={c => this._form = c}
+          type={Report} 
+          options={options}
+        />
+        <Button
+          title="Submit!"
+          onPress={this.handleSubmit}
+        />
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
+    justifyContent: 'center',
+    marginTop: 50,
+    padding: 20,
+    backgroundColor: '#ffffff',
   },
 });
