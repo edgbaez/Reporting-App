f46c61db814fb4459cc26f7462981e2e4b6d2a5e
Daniel Gomez <dggomez21@gmail.com>
2019-03-14 09:28:39 -0400

    Create reporting.js

---
+++ mobile-app/actions/reporting.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+import { Platform } from 'react-native';
+
+let url;
+
+if (Platform.OS !== 'ios') {
+  url = 'http://10.0.3.2:3000/api';
+} else {
+  url = 'http://localhost:3000/api';
+}
+
+axios.defaults.baseURL = url;
+
+class Reporting {
+  constructor(){
+    this.path = `/api`;
+  }
+  
+  async readAllReports(){
+  try{
+  const { data } = await axios.get(this.path); 
+  }
+  cathch(e){
+  throw e;
+  }
+  }
+  async createReport(args) {
+    try {
+      const res = await axios.post(`${this.path}/new`, { ...args });
+      return res;
+    } catch (e) {
+      throw e;
+    }
+  }
+  
+}
+
+export {
+Reporting,
+};
+
